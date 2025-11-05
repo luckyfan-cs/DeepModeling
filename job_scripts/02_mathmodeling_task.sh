07d38fc9914e46fd47a42648f4950012d6e6389a
Your Name <you@example.com>
2025-11-05 08:35:14 +0000

    add data-bench example ds & & science bench

---
+++ job_scripts/02_mathmodeling_task.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+python main.py --workflow scientific --benchmark mathmodeling --data-dir "./data/mathmodeling-bench" --llm-model openai/deepseek-ai/DeepSeek-V3.1-Terminus --task mamo-complex-105
